5b601ffb844afef4413bab103bc5024f86a148dd
BakaBakaGaijin <legostaevofficial@yandex.ru>
2024-02-27 22:58:06 +0300

    Remove useless import

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -2,9 +2,6 @@
 import { ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-/* APPLICATION */
-import "./index.css";
-
 export const withRouter = (component: () => ReactNode) => () => (
   <BrowserRouter>{component()}</BrowserRouter>
 );
